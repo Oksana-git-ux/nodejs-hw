1c293371784ddf3d10995017dd83bf2e296fdae3
Oksana_Melnyk <kc.19870625@gmail.com>
2025-12-02 10:17:27 +0200

    update files errorHandler.js and notesRoutes.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,16 @@
-export const errorHandler = (err, req, res, next) => {
-  const status = err.status || 500;
+import { HttpError } from "http-errors";
 
-  res.status(status).json({
-    message: err.message || "Internal server error",
+export const errorHandler = (err, req, res, next) => {
+
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  res.status(500).json({
+    message: "Internal server error",
   });
 };
